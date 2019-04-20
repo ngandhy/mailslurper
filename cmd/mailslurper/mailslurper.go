174dd6f87761b584dbfd8d0ad01982404eb80df9
ngandhy <ngandhy@users.noreply.github.com>
2019-04-19 22:07:28 -0700

    Update mailslurper.go

--- cmd/mailslurper/mailslurper.go
+++ cmd/mailslurper/mailslurper.go
@@ -14,7 +14,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/labstack/echo@v3.3.9"
+	"github.com/labstack/echo/v3.3.9"
 	"github.com/mailslurper/mailslurper/pkg/mailslurper"
 	"github.com/mailslurper/mailslurper/pkg/ui"
 	"github.com/patrickmn/go-cache"
